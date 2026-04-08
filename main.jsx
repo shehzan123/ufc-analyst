9b3812f4e3f53a42e70293c6344c4a33cbba3169
shehzan123 <75387697+shehzan123@users.noreply.github.com>
2026-04-08 08:51:21 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,4 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-  <rect width="32" height="32" fill="#080808"/>
-  <text x="4" y="24" font-family="serif" font-weight="bold" font-size="22" fill="#C1121F">U</text>
-</svg>
