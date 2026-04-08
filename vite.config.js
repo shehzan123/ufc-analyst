47915ca238f6f234937d28208a21ac7065aa6a4f
shehzan123 <75387697+shehzan123@users.noreply.github.com>
2026-04-08 09:05:24 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,6 @@
-{
-  "name": "ufc-analyst",
-  "version": "1.0.0",
-  "private": true,
-  "scripts": {
-    "dev": "vite",
-    "build": "vite build",
-    "preview": "vite preview"
-  },
-  "dependencies": {
-    "react": "^18.2.0",
-    "react-dom": "^18.2.0"
-  },
-  "devDependencies": {
-    "@vitejs/plugin-react": "^4.2.1",
-    "vite": "^5.1.0"
-  }
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
