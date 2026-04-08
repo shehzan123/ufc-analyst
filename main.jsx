52fb98366723379a8ded74e98a6c9e83a5f76123
shehzan123 <75387697+shehzan123@users.noreply.github.com>
2026-04-08 08:49:16 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,4 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+  <rect width="32" height="32" fill="#080808"/>
+  <text x="4" y="24" font-family="serif" font-weight="bold" font-size="22" fill="#C1121F">U</text>
+</svg>
